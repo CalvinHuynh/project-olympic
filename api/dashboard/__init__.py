404ae4b32dd0d0b430d701e365bbd4ff501082b2
CalvinHuynh <huynhck001@gmail.com>
2019-11-19 23:55:46 +0100

    renamed blueprint to match with project

--- api/dashboard/__init__.py
+++ api/dashboard/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-blueprint = Blueprint('dash-examples_blueprint',
+blueprint = Blueprint('dash_blueprint',
                       __name__,
                       url_prefix='/dash',
                       template_folder='templates')
